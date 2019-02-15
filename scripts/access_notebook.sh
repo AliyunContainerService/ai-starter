7b3c7b0df59226fa878d6a6765d1c6a7b7b614f7
xiaozhouX <allenfromchina@163.com>
2019-02-15 17:03:01 +0800

    init scripts

---
+++ scripts/access_notebook.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+set -e
+ACCESS_TYPE="PORT_FORWARD"
+LABEL="app=arena-notebook"
+DEPLOYMENT_NAME="arena-notebook"
+
+function access() {
+	if [[ "$ACCESS_TYPE" == "SERVICE" ]];then
+		expose_service
+	else
+		port_forward
+	fi
+}
+
+function port_forward() {
+	local NAMESPACE=${NAMESPACE:-default}
+	local PORT=${PORT:-8081}
+	local PODNAME=`kubectl get po -n $NAMESPACE -l $LABEL | grep -v NAME| head -1| awk '{print $1}'`
+	echo "Forwarding pod: $NAMESPACE/$PODNAME, port: $PORT"
+	echo "Open http://localhost:$PORT in browser"
+	kubectl port-forward ${PODNAME} -n ${NAMESPACE} $PORT:8888
+}
+
+function expose_service() {
+	local NAMESPACE=${NAMESPACE:-default}
+	local SERVICE_TYPE=${SERVICE_TYPE:-LoadBalancer}
+	local SERVICE_TYPE_LOW=$(echo $SERVICE_TYPE | tr '[A-Z]' '[a-z]')
+	local SERVICE_NAME=arena-notebook-$SERVICE_TYPE_LOW
+	local SERVICE_URL=$(get_service_url $SERVICE_NAME)
+	if [[ "$SERVICE_URL" != "" ]]; then
+		echo "Service $SERVICE_NAME is exist, You can access notebook by open http://$SERVICE_URL in bowser"
+		echo "If you want to delete the service, please exec \"kubectl delete svc -n $NAMESPACE $SERVICE_NAME\" "
+		exit 0
+	fi
+
+	kubectl expose deployment $DEPLOYMENT_NAME -n $NAMESPACE --type=$SERVICE_TYPE --name=$SERVICE_NAME
+	echo "Expose arena notebook by $SERVICE_TYPE type service, you can exec \"kubectl get svc -n $NAMESPACE $SERVICE_NAME\" to check the service."
+	echo "If you want to delete the service, please exec \"kubectl delete svc -n $NAMESPACE $SERVICE_NAME\" "
+	if [[ "$SERVICE_TYPE" == "LoadBalancer" ]]; then
+		echo "Wait for loadbalancer ready..."
+	fi
+	SERVICE_URL=$(get_service_url $SERVICE_NAME)
+	while [[ $SERVICE_URL == "" ]];do
+		sleep 3
+		SERVICE_URL=$(get_service_url $SERVICE_NAME)
+	done
+	echo "You can access notebook by open http://$SERVICE_URL in bowser"
+}
+
+function get_service_url() {
+	local SERVICE_NAME=$1
+	local NAMESPACE=${2:-default}
+	set +e
+	kubectl get svc -n $NAMESPACE $SERVICE_NAME &> /dev/null
+	local exist=$?
+	set -e
+	if [[ $exist == 1 ]]; then
+		echo ""
+	else
+		SERVICE_TYPE=$(kubectl get svc -n $NAMESPACE $SERVICE_NAME -ojsonpath='{.spec.type}')
+		SERVICE_PORT=$(kubectl get svc -n $NAMESPACE $SERVICE_NAME -ojsonpath='{.spec.ports[0].port}')
+		SERVICE_IP=""
+		if [[ "$SERVICE_TYPE" == "NodePort" ]];then
+			SERVICE_IP="<Node-IP>"
+		elif [[ "$SERVICE_TYPE" == "LoadBalancer" ]]; then
+			SERVICE_IP=$(kubectl get svc -n $NAMESPACE $SERVICE_NAME -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
+		else
+			SERVICE_IP=$(kubectl get svc -n default $SERVICE_NAME -ojsonpath='{.spec.clusterIP}')
+		fi
+		
+		if [[ $SERVICE_IP == "" ]];then
+			echo ""
+			exit 0
+		fi
+		echo "$SERVICE_IP:$SERVICE_PORT"
+	fi
+}
+
+usage() {
+    echo "Usage:"
+    echo "  access_notebook.sh [-s] [-p] [-t SERVICE_TYPE]"
+    echo "Options:"
+    echo "    -p, use port forward to access notebook.[default]"
+    echo "    -s, use service to access notebook."
+    echo "    -t, the type of service. LoadBalancer/NodePort/ClusterIP"
+    exit -1
+}
+
+function main() {
+	while [ $# -gt 0 ];do
+	    case $1 in
+	        -p|--port-forward)
+	            ACCESS_TYPE="PORT_FORWARD"
+	            ;;
+	        -s|--service)
+	            ACCESS_TYPE="SERVICE"
+	            ;;
+	        -t|--service-type)
+	            SERVICE_TYPE=$2
+	            shift
+	            ;;
+	        -h|--help)
+	            usage
+	            exit 0
+	            ;;
+	        *)
+	            echo "unknown option [$key]"
+	            exit 1
+	        ;;
+	    esac
+        shift
+	done
+	access
+}
+
+main "$@"
---
+++ scripts/install_arena.sh
@@ -0,0 +1,72 @@
+function install() {
+	HOST_NETWORK=${HOST_NETWORK:-"false"}
+	PROMETHEUS=${PROMETHEUS:-"false"}
+	NAMESPACE=${NAMESPACE:-"default"}
+
+	cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: arena-installer
+  namespace: $NAMESPACE
+  labels:
+    app: arena-installer
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: arena-installer
+  template:
+    metadata:
+      labels:
+        app: arena-installer
+    spec:
+      containers:
+      - name: arena
+        image: cheyang/arena:0.2.0
+        env:
+        - name: useHostNetwork
+          value: "$HOST_NETWORK"
+        - name: usePrometheus
+          value: "$PROMETHEUS"
+        - name: platform
+          value: ack
+        - name: KUBECONFIG
+          value: /root/.kube/config
+        volumeMounts:
+        - mountPath: /root/.kube/config
+          name: kube-config
+      volumes:
+        - name: kube-config
+          hostPath:
+            path: /root/.kube/config
+            type: File
+EOF
+}
+
+
+
+function main() {
+	while [ $# -gt 0 ];do
+	    case $1 in
+	        -p|--prometheus)
+	            PROMETHEUS="true"
+	            ;;
+	        -h|--host-network)
+	            HOST_NETWORK="true"
+	            ;;
+	        -n|--namespace)
+	            NAMESPACE=$2
+	            shift
+	            ;;
+	        *)
+	            echo "unknown option [$key]"
+	            exit 1
+	        ;;
+	    esac
+        shift
+	done
+	install
+}
+
+main "$@"
